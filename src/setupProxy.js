e4cea998c5f83bc6b540edcc9fcd9834a0c91d98
Omar Rodriguez <omarrodriguez@Omars-MacBook-Pro.local>
2023-10-22 12:24:17 -0600

    change cross origin

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth", "graphql"], {
-      target: `http://localhost:${process.env.BACKEND_PORT}`,
+      target: `https://qabackend.vercel.app`,
       changeOrigin: true,
       logLevel: "debug",
     })
